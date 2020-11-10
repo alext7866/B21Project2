c0e9b85879795a4a9513cbb4fe5ac29690d81f72
Alex Tsegaye <alext7866@gmail.com>
2020-11-10 11:41:58 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello");
+
+    }
+}
